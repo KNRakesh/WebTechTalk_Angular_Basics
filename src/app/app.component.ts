1cb4e3870c241a48c6fcb2b87d43dd619d59e70c
Rakesh KN <rakeshshandilyaa@gmail.com>
2025-04-23 20:23:18 +0530

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { BodyComponent } from './body/body.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    BodyComponent,
+    SideNavComponent,
+    MainBodyComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'my-first-app';
